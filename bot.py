78a718a9c970019e98a8c0d43ea37a33c2ca4e26
Zicnet <90823049+Zicnet@users.noreply.github.com>
2023-05-30 12:45:23 -0600

    Bot Update
    Add Bot

---
+++ bot.py
@@ -0,0 +1,107 @@
+import disnake
+import datetime
+import mysqlrequests
+import mysql.connector
+from message import messages
+from mysqlrequests import User
+from datetime import datetime
+from disnake.ext import commands
+from config import settings
+from mysqlconfig import host, user, password, db_name
+
+
+
+balance = mysqlrequests.User.balance
+political_opinion = mysqlrequests.User.political_opinion
+
+intents = disnake.Intents.default()
+test_guilds=[1112211843914670100]
+intents.voice_states = True
+
+bot = commands.Bot(
+    command_prefix=settings['prefix'],
+    help_command=None,
+    intents=disnake.Intents.all(),
+    activity=disnake.Game('Zicnet'),
+    status=disnake.Status.streaming
+)
+con = mysql.connector.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=db_name
+)
+cur = con.cursor()
+
+# bot event
+
+@bot.event
+async def on_ready():
+    print(f"{datetime.now()} Bot ready")
+
+# bot command
+
+@bot.slash_command(guild_ids=test_guilds,
+                    name='register',
+                    description='Registration')
+async def register(ctx, opponent: disnake.Member, political_opinion = disnake.Role):
+    member = ctx.author
+    role = disnake.utils.get(ctx.guild.roles, id=political_opinion)
+    await opponent.add_roles(role)
+    reply = mysqlrequests.User.reply
+    client = mysqlrequests.User(member.id)
+    if client.check == True:
+        await client.reply(ctx, False, 'Регистрация', 'regerror')
+        return
+
+    cur = con.cursor()
+    cur.execute(f"INSERT INTO user(discord_id,balance,political_opinion,date_registrator) VALUES({opponent.id}, '0', '{political_opinion}','{datetime.now().date()}')")
+    con.commit()
+    cur.close()
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(f"{datetime.now()} Bot start")
+bot.run(settings['token'])
---
+++ mysqlrequests.py
@@ -0,0 +1,59 @@
+import disnake
+import mysql.connector
+from message import messages
+from mysqlconfig import host, user, password, db_name
+
+con = mysql.connector.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=db_name
+)
+cur = con.cursor()
+
+
+class User:
+    def __init__(self, discordID):
+        cur = con.cursor()
+        cur.execute(f"SELECT id, discord_id, balance, political_opinion, date_registrator FROM user WHERE discord_id = {discordID}")
+        record = cur.fetchone()
+
+        if record is None:
+            self.check = False
+            return
+        
+        self.check = True   
+        self.id = record[0]
+        self.discord_id = record[1]
+        self.balance = record[2]
+        self.political_opinion = record[3]
+        self.date_registrator = record[4]
+
+
+    def balance(self, money):
+        cur = con.cursor()
+        cur.execute(f"UPDATE user SET balance = '{money}' WHERE discord_id = {self.discord_id}")
+        con.commit()
+        cur.close()
+
+    def political_opinion(self, string):
+        cur = con.cursor()
+        cur.execute(f"UPDATE user SET political_opinion = '{string}' WHERE discord_id = {self.discord_id}")
+        con.commit()
+        cur.close()
+
+    async def reply(self,ctx, redgreen, head, text):
+        if text in messages:
+            text = messages[text]
+        if redgreen:
+            color = disnake.Colour.from_rgb(51, 153, 102)
+        else:
+            color = disnake.Colour.from_rgb(255, 102, 102)
+        embed = disnake.Embed(
+            title=head,
+            description=text,
+            colour=color
+        )
+        await ctx.response.send_message(embed=embed)
+
+        return
