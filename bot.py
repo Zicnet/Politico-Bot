9905c3e8f4f863023fe65925794343ef7f56645b
Zicnet <90823049+Zicnet@users.noreply.github.com>
2023-05-30 13:47:00 -0600

    UPDATE 30.05/13.47

--- bot.py
+++ bot.py
@@ -45,20 +45,22 @@ async def on_ready():
 @bot.slash_command(guild_ids=test_guilds,
                     name='register',
                     description='Registration')
-async def register(ctx, opponent: disnake.Member, political_opinion = disnake.Role):
+async def register(ctx, opponent: disnake.User, political_opinion: disnake.Role):
     member = ctx.author
-    role = disnake.utils.get(ctx.guild.roles, id=political_opinion)
-    await opponent.add_roles(role)
+    role = disnake.utils.get(ctx.guild.roles, id=political_opinion.id)
+
     reply = mysqlrequests.User.reply
     client = mysqlrequests.User(member.id)
     if client.check == True:
         await client.reply(ctx, False, 'Регистрация', 'regerror')
         return
-
+    
+    await opponent.add_roles(role)
     cur = con.cursor()
-    cur.execute(f"INSERT INTO user(discord_id,balance,political_opinion,date_registrator) VALUES({opponent.id}, '0', '{political_opinion}','{datetime.now().date()}')")
+    cur.execute(f"INSERT INTO user(discord_id,balance,political_opinion,date_registrator) VALUES({opponent.id}, '0', '{political_opinion.id}','{datetime.now().date()}')")
     con.commit()
     cur.close()
+
 
 print(f"{datetime.now()} Bot start")
 bot.run(settings['token'])
