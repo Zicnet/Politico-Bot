67f578e5e0f9f54f26b1aa63dcd95fd4f6808bab
KiraPixel <thepowerkira@gmail.com>
2023-05-31 00:09:34 +0300

    mysql update

--- bot.py
+++ bot.py
@@ -14,9 +14,8 @@ import discord_reply
 balance = mysqlrequests.User.balance
 political_opinion = mysqlrequests.User.political_opinion
 intents = disnake.Intents.default()
-test_guilds=[1112211843914670100]
+test_guilds = [1112211843914670100]
 intents.voice_states = True
-
 
 # Bot config and sql connector
 bot = commands.Bot(
@@ -36,18 +35,29 @@ async def on_ready():
 
 # bot command
 @bot.slash_command(guild_ids=test_guilds,
-                    name='register',
-                    description='Registration')
-async def register(ctx, opponent: disnake.Member, political_opinion = disnake.Role):
-    member = ctx.author # получаем чела
-    role = disnake.utils.get(ctx.guild.roles, id=political_opinion)     # получаем объект роли, для вписанной роли
+                   name='register',
+                   description='Registration')
+async def register(ctx, opponent: disnake.Member, role: disnake.Role):
+    member = ctx.author  # получаем чела
     client = mysqlrequests.User(member.id)
     if client.check:
         await discord_reply.reply(ctx, False, 'Регистрация', 'regerror')
         return
-    client.db_register(political_opinion)   # регаем в базе
+    client.db_register(role.id)  # регаем в базе
     await opponent.add_roles(role)  # какидываем роль
-    # добавить reply из discord_reply когда зарегали чела
+    await discord_reply.reply(ctx, True, 'Регистрация', 'regesuc')
+
+
+@bot.slash_command(guild_ids=test_guilds,
+                   name='info',
+                   description='Registration')
+async def info(ctx, member: disnake.Member):
+    member = ctx.author
+    client = mysqlrequests.User(member.id)
+    if not client.check:
+        await discord_reply.reply(ctx, False, 'Регистрация', 'regerror')
+        return
+    await discord_reply.reply_info(ctx, 'Info')
 
 
 print(f"{datetime.now()} Bot start")
